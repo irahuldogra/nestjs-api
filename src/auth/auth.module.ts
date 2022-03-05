809e46e238f1486a309b775f5392fa74fa3c48ba
Rahul Dogra <rahuldogra1998@gmail.com>
2022-03-05 18:04:46 +0530

    secure: added jwt generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
-import { PrismaModule } from './../prisma/prisma.module';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { Module } from '@nestjs/common';
+
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
